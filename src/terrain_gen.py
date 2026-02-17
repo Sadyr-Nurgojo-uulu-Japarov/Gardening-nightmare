ae851ea79d1be005b257e91e78c02d2db2693950
Tony <2fflofabre@gmail.com>
2026-02-17 22:37:44 +0100

    a

--- src/terrain_gen.py
+++ src/terrain_gen.py
@@ -7,10 +7,12 @@ class TerrainGenClass:
         terrain.TILE_SIZE = 75
         terrain.x, terrain.y = 0, 0
         terrain.PLAYER_SPEED = 5
-        terrain.ModifiedTiles = {(2, 2): (255, 0, 0),
-                                 (5, 3): (0, 0, 255),
-                                 (10, 10): (255, 255, 0),
-                                 (-2, -2): (255, 255, 255)}
+        terrain.ModifiedTiles = {
+            (2, 2): (255, 0, 0),
+            (5, 3): (0, 0, 255),
+            (10, 10): (255, 255, 0),
+            (-2, -2): (255, 255, 255)
+            }
 
     def move_player(terrain, keys):
 
@@ -39,7 +41,7 @@ class TerrainGenClass:
                 if (tileX, tileY) in terrain.ModifiedTiles:
                     color = terrain.ModifiedTiles[(tileX, tileY)]
                 else:
-                    color = (0, 255, 0)  # Default green color for unmodified tiles
-
+                    color = (0, 255, 0)
+                    
                 pygame.draw.rect(screen, color, (drawX, drawY, terrain.TILE_SIZE, terrain.TILE_SIZE))
                 pygame.draw.rect(screen, (0, 200, 0), (drawX, drawY, terrain.TILE_SIZE, terrain.TILE_SIZE), 1)
