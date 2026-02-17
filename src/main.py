dd205b97aa48d8b8d2056649eafd8b7defe37cf7
Guillaume <guillaume2aix.dly@gmail.com>
2026-02-17 15:41:13 +0100

    FAAAAAAAAAAAAH, moving cube

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 class Game:
     def __init__(self):
         # Pygame setup
@@ -12,15 +11,42 @@ class Game:
         self.running = True
 
     def update(self):
+        pressed_keys = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 self.running = False
+        player.move_player(pressed_keys)
 
     def draw(self):
-        pass
+        game.screen.fill("black")
+        player.draw_player()
 
+class Player:
+    def __init__(self):
+        self.x = 1280
+        self.y = 720
+        self.size = 40
+        self.speed = 5
+        self.health = 100
+        self.damage_cooldown = 0
+        self.movement_keys = [pygame.K_z,pygame.K_d,pygame.K_s,pygame.K_q]
+
+    def move_player(self,pressed_key):
+        print(pressed_key)
+        if pressed_key[self.movement_keys[0]]:
+            self.y -= self.speed
+        if pressed_key[self.movement_keys[1]]:
+            self.x += self.speed
+        if pressed_key[self.movement_keys[2]]:
+            self.y += self.speed
+        if pressed_key[self.movement_keys[3]]:
+            self.x -= self.speed
+
+    def draw_player(self):
+        pygame.draw.rect(game.screen,"blue",[self.x,self.y,self.size,self.size])
 
 game = Game()
+player = Player()
 while game.running:
     game.update()
     game.draw()
