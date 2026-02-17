b5b9016ee21acbe75a957aa7ca9bc081737a3e45
Tony <2fflofabre@gmail.com>
2026-02-17 17:23:56 +0100

    fixed player movement

--- src/enemy.py
+++ src/enemy.py
@@ -10,18 +10,16 @@ class EnemyClass:
     
     def update(enemy):
         # Move towards the player
-        diff_x = enemy.x - enemy.target.x
-        diff_y = enemy.y - enemy.target.y
-        maxi = max(abs(diff_x),abs(diff_y))
-        mini = min(abs(diff_x),abs(diff_y))
-        ratio = maxi/mini
-        if maxi == abs(diff_x):
-            
-            enemy.x += enemy.speed * ratio * -1 if diff_x > 0 else enemy.speed * ratio
-            enemy.y += enemy.speed
-        else:
-            enemy.x += enemy.speed
-            enemy.y += enemy.speed * ratio * -1 if diff_y > 0 else enemy.speed * ratio
+        diff_x = enemy.target.x - enemy.x
+        diff_y = enemy.target.y - enemy.y
+
+        distance = (diff_x ** 2 + diff_y ** 2) ** 0.5
+        if distance != 0:
+            enemy.x +=  diff_x / distance * enemy.speed
+            enemy.y += diff_y / distance * enemy.speed
+
+
+        
 
     def draw(enemy, screen):
         pygame.draw.rect(screen, (255, 0, 0), [enemy.x, enemy.y, 20, 20])
