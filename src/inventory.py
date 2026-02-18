eef6cfe50534c20fdc425bf18d16c65aa0b736db
Guillaume <guillaume2aix.dly@gmail.com>
2026-02-18 17:10:27 +0100

    Centered the object in the hotbar

--- src/inventory.py
+++ src/inventory.py
@@ -12,15 +12,18 @@ class InventoryClass:
         # Loading all object sprites AW GAAAWWWD
         hoe_image = pygame.image.load("assets/item/item8.png").convert_alpha()
         hoe_image = pygame.transform.scale(hoe_image,(inventory.SLOT_SIZE*0.7,inventory.SLOT_SIZE*0.7))
-        hoe_image = pygame.transform.rotate(hoe_image,-45)
         inventory.AllObjects = {"Hoe":hoe_image}
 
     def draw_hotbar(inventory,screen):
         for i in range(9):
-            screen.blit(inventory.slot,(inventory.SLOT_SIZE*5 + i*inventory.SLOT_SIZE,screen.get_rect()[3]-inventory.SLOT_SIZE-50),(235*inventory.ratio,11*inventory.ratio,26*inventory.ratio,24*inventory.ratio))
+            x = inventory.SLOT_SIZE*5 + i*inventory.SLOT_SIZE
+            y = screen.get_rect()[3]-inventory.SLOT_SIZE-50
+            screen.blit(inventory.slot,(x,y),(235*inventory.ratio,11*inventory.ratio,26*inventory.ratio,24*inventory.ratio))
             if inventory.hotbar[i] is None:
                 pass
             else:
-                print((inventory.SLOT_SIZE*5 + i*inventory.SLOT_SIZE+inventory.SLOT_SIZE//10,screen.get_rect()[3]-inventory.SLOT_SIZE-40),pygame.mouse.get_pos())
-                screen.blit(inventory.AllObjects["Hoe"],(inventory.SLOT_SIZE*5 + i*inventory.SLOT_SIZE+inventory.SLOT_SIZE//10,screen.get_rect()[3]-inventory.SLOT_SIZE-40))
+                hoeImage = inventory.AllObjects[inventory.hotbar[i]]
+                hoeX = inventory.SLOT_SIZE*5 + i*inventory.SLOT_SIZE + (inventory.SLOT_SIZE - hoeImage.get_rect()[2])/2
+                hoeY = screen.get_rect()[3]-inventory.SLOT_SIZE-50 + (inventory.SLOT_SIZE - hoeImage.get_rect()[3])/2
+                screen.blit(hoeImage,(hoeX,hoeY))
 
